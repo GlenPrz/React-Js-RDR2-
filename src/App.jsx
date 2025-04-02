336ada323587bedd62c8ee6e5fa3e060c52c1e27
GlenPrz <glenwilliam.perez36@gmail.com>
2025-04-02 16:33:24 +0800

    modified:   src/App.jsx 	modified:   src/components/MyButtonComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
+import React from 'react';
+import {MyButtonComponent, CardTest} from './components/MyButtonComponent';
+
 function App() {
 
   const cities =["Taytay", "Taguig", "Angono", "Manila"];
   const newCities = cities.filter((city) => city !== "Manila");
 
   const person = [
-    {
-      id:0,
-      name: "Mark",
-      age: 24,
-    },
-    {
-      id:1,
-      name: "Daves",
-      age: 23,
-    },
-    {
-      id:2,
-      name: "Glenski",
-      age: 22,
-    },
-    {
-      id:3,
-      name: "Jays",
-      age: 21,
-    },
+    // {
+    //   id:0,
+    //   name: "Mark",
+    //   age: 24,
+    // },
+    // {
+    //   id:1,
+    //   name: "Daves",
+    //   age: 23,
+    // },
+    // {
+    //   id:2,
+    //   name: "Glenski",
+    //   age: 22,
+    // },
+    // {
+    //   id:3,
+    //   name: "Jays",
+    //   age: 21,
+    // },
   ];
+
 
 
   return (
     <div>
 
       <h1>City List</h1>
+      <br />
       <ul className="list-group">
         {newCities.map((city, index) => (
           <li className="list-group-item" key={index}>
@@ -38,7 +43,8 @@ function App() {
             </li>
         ))}
       </ul>
-      
+      <br />
+      <br />
       <hr />
 
       <h1>Table</h1>
@@ -48,19 +54,34 @@ function App() {
             <th scope="col">Id</th>
             <th scope="col">Name</th>
             <th scope="col">Age</th>
+            <th scope="col">Stats</th>
           </tr>
         </thead>
         <tbody>
-          {person.map((person) => (
+          {person.length !== 0 ?
+          (person.map((person) => (
             <tr key={person.id}>
               <td>{person.id}</td>
               <td>{person.name}</td>
               <td>{person.age}</td>
+              <td>{person.age > 22 ? "old" : "young"}</td>
             </tr>
-          ))}
+          )))
+          :
+          (<tr>
+             <td colSpan="4">No data available</td>
+          </tr>)}
+          
 
         </tbody>
       </table>
+
+      <br />
+      <MyButtonComponent />
+      <br />
+
+      <CardTest />
+      <br />
     </div>
   )
 }
--- src/components/MyButtonComponent.jsx
+++ src/components/MyButtonComponent.jsx
@@ -7,6 +7,18 @@ const MyButtonComponent = () => {
 )
 }
 
-export default MyButtonComponent;
+const CardTest = () => {
+  return (
+    <div className="card" style={{width: "18rem"}}>
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  )
+}
+export {MyButtonComponent, CardTest};
 
 
